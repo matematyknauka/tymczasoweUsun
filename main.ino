e9d137cffd2ac1d3c94d2b0bcab5ba178468bf98
matematyknauka <matematyknauka@gmail.com>
2026-09-26 18:26:07 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,6 +2,14 @@
 
 void setup() {
   set_up();
+  // Wyświetlanie tekstów
+    setCursor(3, 0);
+    printLCD("Godzina");
+
+    setCursor(0, 1);
+    char buffer[16];
+    sprintf(buffer, "%02d:%02d", 2, 54);
+    printLCD(buffer);
 }
 
 void loop() {
